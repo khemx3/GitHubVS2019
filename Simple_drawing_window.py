82b304ec3ad9f6a754610ad48bebbe3940426482
ppongnp <ppgxnp@gmail.com>
2019-03-28 13:38:38 +0700

    fix QWidget problem

--- Simple_drawing_window.py
+++ Simple_drawing_window.py
@@ -3,7 +3,7 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 
-class Simple_drawing_window(Qwidget):
+class Simple_drawing_window(QWidget):
     def __init__ (self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
