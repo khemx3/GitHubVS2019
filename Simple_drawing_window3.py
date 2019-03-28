ae908c7b7c4730bfa9949b7da9f41a80b413fd64
sarin_beam30 <sarin_beam30@hotmail.com>
2019-03-28 14:05:16 +0700

    Final_beam

--- Simple_drawing_window3.py
+++ Simple_drawing_window3.py
@@ -3,7 +3,7 @@ from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 
-class Simple_drawing_window(QWidget):
+class Simple_drawing_window3(QWidget):
     def __init__ (self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
@@ -16,18 +16,25 @@ class Simple_drawing_window(QWidget):
 
         p.setPen(QColor(0, 0, 0))
         p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([
-            QPoint( 70, 100), QPoint(100, 110),
-            QPoint( 130, 100), QPoint(100, 150),
-        ])
+        
+
+        p.setPen(QColor(127, 127, 0))
+        p.setBrush(QColor(150, 127, 127))
+        p.drawLine(20, 240, 180, 250)
+        p.drawLine(20,240,120,50)
+        p.drawLine(120,50,180,250)
+        p.drawRect(20, 240, 155, 250)
+        #p.fillRect(20,240,160,100, qRed)
+
+        
 
         p.setPen(QColor(255, 127, 0))
         p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+        #p.drawPie(50, 150, 100, 100, 0, 180 * 16)
 
-        p.drawPolygon(
-            [QPoint( 50, 200), QPoint(150, 200), QPoint(100, 400), ]
-            )
+        
+        
+        
 
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
